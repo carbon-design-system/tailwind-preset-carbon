6efc78642fedd21e839b0a869a80f4961f015424
Josh Black <josh@josh.black>
2021-05-30 15:26:29 -0500

    fix(examples): include src in purgecss rules (#15)

--- examples/nextjs/tailwind.config.js
+++ examples/nextjs/tailwind.config.js
@@ -9,5 +9,8 @@
 
 module.exports = {
   presets: [require('../../')],
-  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+  purge: [
+    './src/pages/**/*.{js,ts,jsx,tsx}',
+    './src/components/**/*.{js,ts,jsx,tsx}',
+  ],
 };
